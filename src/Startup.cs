7c322f845884300dddb3339fcb06ea2b3bbdd314
Bezkup <bruroy96@gmail.com>
2019-12-18 09:51:19 +0100

    Aggiunte view di contenuto e una view di layout

--- src/Startup.cs
+++ src/Startup.cs
@@ -16,6 +16,7 @@ namespace CorsoDotNet
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,14 +35,10 @@ namespace CorsoDotNet
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    string nome = context.Request.Query["nome"];
-
-                    await context.Response.WriteAsync($"Hello {nome.ToUpper()}!");
-                });
+            //app.UseMvcWithDefaultRoute();
+            
+            app.UseEndpoints(route => {
+                route.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
