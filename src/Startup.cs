3c90fc031322e229ec22f6342bd365151544a012
Bezkup <bruroy96@gmail.com>
2019-12-18 14:30:22 +0100

    Struttura elenco pagina corsi

--- src/Startup.cs
+++ src/Startup.cs
@@ -16,7 +16,12 @@ namespace CorsoDotNet
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+
+            #if DEBUG
+            .AddRazorRuntimeCompilation()
+            #endif
+            ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +29,12 @@ namespace CorsoDotNet
         {
            // if (env.IsDevelopment())
 
-           app.UseHttpsRedirection();
+        //    app.UseHttpsRedirection();
            
-            if(env.IsEnvironment("Development"))
+            if(env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                
             }
 
             app.UseStaticFiles();
