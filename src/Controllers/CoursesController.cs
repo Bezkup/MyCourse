6c1513d766ec5add6bcbdf70b5db41885126f3a8
Bezkup <bruroy96@gmail.com>
2019-12-18 09:54:43 +0100

    Commit precedente

--- src/Controllers/CoursesController.cs
+++ src/Controllers/CoursesController.cs
@@ -6,12 +6,12 @@ namespace src.Controllers
     {
         public IActionResult Index(){
     
-            return Content("Sono Index");
+            return View();
         }
         
         public IActionResult Detail(string id){
     
-            return Content($"Sono detail, ho ricevuto l'id : {id}");
+            return View();
         }
 
         public IActionResult Search(string title){
